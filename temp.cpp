c2a2fc9d81f1ec7867ebcd6fda93f70a9e36f807
Rajeshwar Rao <rajeshwarr055@gmail.com>
2017-01-09 20:50:53 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%lld",&x)
+
+#define all(x) x.begin(),x.end()
+#define mp make_pair
+
+#define LL long long
+#define ll long long
+#define ld long double
+#define pb push_back
+
+#define MOD 1000000007
+
+#define pi 2*acos(0)
+
+#define fr first
+#define se second
+#define vi vector < int >
+#define pii pair < int , int >
+#define pll pair < ll , ll >
+
+#define MEMSET_INF 127
+#define mem(a,x) memset(a , x , sizeof ( a ))
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(NULL)
+
+
+int main(){
+int t;
